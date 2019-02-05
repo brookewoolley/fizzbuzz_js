d0b6103bb8584b01e540f23f09cdd62ccc745c52
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-05 12:22:16 +0000

    Passing tests for divisible functions

--- jasmine-standalone-3.3.0/src/fizzBuzz.js
+++ jasmine-standalone-3.3.0/src/fizzBuzz.js
@@ -1,6 +1,17 @@
-var Fizzbuzz = function() {
+var Fizzbuzz = function() {};
+
+Fizzbuzz.prototype._isDivisibleBy = function(num, divisor) {
+ return (num % divisor === 0);
+};
 
 Fizzbuzz.prototype.isDivisibleByThree = function(num) {
- return (num % 3 === 0);
-};
-};
+  return this._isDivisibleBy(num, 3);
+}
+
+Fizzbuzz.prototype.isDivisibleByFive = function(num) {
+  return this._isDivisibleBy(num, 5);
+}
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(num) {
+  return this._isDivisibleBy(num, 15);
+}
